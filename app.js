78307d072dc15262f9093dc568dddf8f6d2f47d6
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-04-13 16:35:46 +0530

    feat: navbar js started

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector(".navbar__menu");
-
-//display mobile menu
+const navLogo = document.querySelector('#navbar__logo')
+    //display mobile menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 };
 
 menu.addEventListener('click', mobileMenu);
+
+//show active menu when scrolling
+const highLightMenu = () => {
+    const elem = document.querySelector(.
+        'higlight');
+    const elem = document.querySelector(.
+        'higlight');
+    const elem = document.querySelector(.
+        'higlight');
+    const elem = document.querySelector(.
+        'higlight');
+}
