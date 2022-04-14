e9304907c7d1b08b5d9e0271fa1b1d72cc8b5eaf
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-04-14 19:38:57 +0530

    feat: highlight js

--- app.js
+++ app.js
@@ -11,8 +11,21 @@ menu.addEventListener('click', mobileMenu);
 
 //show active menu when scrolling
 const highlightMenu = () => {
-    const element = document.querySelector('.highlight');
+    const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
-    const servicesMenu = document.querySelector('#services-page');
+    const servicesMenu = document.querySelector("services-page");
+    let scrollPos = window.scrollY
+
+    // adds 'highlight' class to my menu items
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else(window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+    }
 }
