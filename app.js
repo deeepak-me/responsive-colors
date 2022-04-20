036a527e3cf88de8b583a2ec1710bf4e2fd5e4d8
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-04-21 01:18:00 +0530

    feat: js part continues

--- app.js
+++ app.js
@@ -23,12 +23,18 @@ const highlightMenu = () => {
         homeMenu.classList.add('highlight')
         aboutMenu.classList.remove('highlight')
         return
-    } else(window.innerWidth > 960 && scrollPos < 1400) {
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
         aboutMenu.classList.add('highlight')
         homeMenu.classList.remove('highlight')
         servicesMenu.classList.remove('highlight')
         return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
     }
 
-    if ((elem))
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight')
+    }
 }
